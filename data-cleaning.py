b6875d697cc4c3262d0383d8834850397970b553
kaleighspitzer <spitzer4@illinois.edu>
2021-06-25 13:57:22 -0400

    Changed indexing to be by state

--- data-cleaning.py
+++ data-cleaning.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 
-data_frame = pd.read_csv('data/covid-data.csv')
-# data_frame.head()
 
+data_frame = pd.read_csv('data/covid-data.csv')
+
+# Remove unnecessary columns
 columns_to_drop = ['Total Cases/1 mil population', 'Death/1 mil population', 'Total Cases.1', 'Tests/1 mil population']
 data_frame.drop(columns=columns_to_drop, inplace=True)
+
+# Set index to State
+data_frame = data_frame.set_index('State')
+
+
