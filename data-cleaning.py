609212bd2bd11ef0fcd2fdbdc7385908e307374b
kaleighspitzer <spitzer4@illinois.edu>
2021-06-25 14:18:25 -0400

    Removed commas from data and changed column types to numeric

--- data-cleaning.py
+++ data-cleaning.py
@@ -11,4 +11,16 @@ data_frame.drop(columns=columns_to_drop, inplace=True)
 # Set index to State
 data_frame = data_frame.set_index('State')
 
+# Remove commas from numerical data
+total_cases = data_frame['Total Cases'].str.replace(',', '')
+total_deaths = data_frame['Total Deaths'].str.replace(',', '')
+total_recovered = data_frame['Total Recovered'].str.replace(',', '')
+active_cases = data_frame['Active Cases'].str.replace(',', '')
+population = data_frame['Population'].str.replace(',', '')
 
+# Set column types to numeric
+data_frame['Total Cases'] = pd.to_numeric(total_cases)
+data_frame['Total Deaths'] = pd.to_numeric(total_deaths)
+data_frame['Total Recovered'] = pd.to_numeric(total_recovered)
+data_frame['Active Cases'] = pd.to_numeric(active_cases)
+data_frame['Population'] = pd.to_numeric(population)
